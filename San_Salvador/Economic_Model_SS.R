66039ff8b1931c1feb3b1f61398a5e3e19c60429
AugustoSouto <augustosoutoperez@gmail.com>
2020-11-11 17:28:46 -0300

    Commit

--- San_Salvador/Economic_Model_SS.R
+++ San_Salvador/Economic_Model_SS.R
@@ -21,30 +21,6 @@ load(scenario)
 
 }
 
-
-hru_info <- 
-  plyr::join(areas, hrus_rotations, by="hru"); head(hru_info)
-
-#area por rotacion-
-hru_info %>% group_by(lu_mgt) %>% 
-  summarise(area_rot=sum(area)) %>%
-  mutate(pct_rot=area_rot/sum(area_rot)) %>%
-write_excel_csv2(paste0("Areas_Rot_", 
-                        str_remove( scenario, ".RData"),
-                        ".csv"))
-    
-hru_info %>% group_by(lu_mgt) %>% 
-  summarise(area_rot=sum(area), 
-            area_prom=mean(area),
-            area_mediana=median(area),
-            area_max=max(area),
-            area_min=min(area), 
-            hrus=n()) %>%
-  write_excel_csv2(paste0("Areas_Rot_", 
-                          str_remove( scenario, ".RData"),
-                          ".csv"))
-
-
 #environmental_output$P_Concentration
 
 
@@ -83,16 +59,8 @@ ifelse(Excess_Days>0.1,"TRUE","FALSE") %>%
 #save data
 setwd("C:/Users/Usuario/Desktop/Git/Tesis")
 
-saveRDS(paste0(str_remove(scenarios[i], ".RData"), "_profits.RDS"))
-
-readline(prompt = "Press any key only if management scenarios are done and saved")
-
-
 save.image("Economic_Profit.RData")
 #Utility Functions----
-
-
-
 
 alpha <- 0.88 #Parameters from Kahneman-Tversky and 
               #Adopted by Rosas, Sans and Arana
@@ -101,111 +69,203 @@ alpha <- 0.88 #Parameters from Kahneman-Tversky and
 #by Booij, Van Praag and Kuilen 2009
 #alpha range:0.22 (Camerer and ho 1994) to 1.01 (Fehr-Duda 2006)
 
-lambda <- 2.25 #Parameters from Kahneman-Tversky and 
-               #Adopted by Rosas, Sans and Arana
-#Lambda range in Booij et al is 1.07 to 3.2
-
 #interpretation: alpha defines the curvature
-#               of ut function 
-#               lambda is the risk aversion, defined
-#               as lambda=-u(-1)/u(1)
-
-
-
+#                of ut function 
 
 #vector profit para probar
 coso <- profit_data %>% select(profit_ha,yr ) %>% filter(hru==3)  %>% 
         as.data.frame() %>%  select(profit_ha)
 
-#cara utility in one period
-cara_utility <- function(value, rho=0.5){
-  ifelse(rho!=0, ((1-exp(-rho*value))/rho), log(value))
+ne_utility <- function(value, rho=0.005){
+  
+  utility<-  -exp(-rho*value)
+  return(utility)
 }
-#discount cara utilities to t=1
-disc_cara_utility <- function(profit_vector){
-  
 
-  utilities <- apply(profit_vector, 1, cara_utility)
+pow_utility <- function(value, rho=0.5){
+  
+  utility<- value^(1-rho)
+  
+  return(utility)
+}
 
-  utility_discount <- 0.075 #see how to calibrate this, meanwhile, use social disc rate
-  
-  #set the discount rate vector r
-  r <- vector(length = dim(profit_vector)[1]) 
+#cara utility for the hru in the whole period
+ne_utility_whole <- function(profit_vector, rho=0.005){
   
   
+  utility<- sapply(profit_vector, function(x){-exp(-rho*x)}) %>% sum()
   
-  for (i in 2:length(r)) {
-    r[1]<-1
-    r[i]<-(1/(1+utility_discount))^(i-1) 
-  }
-   
-  mat_r <- matrix(0, ncol=length(r), nrow=length(r))
-  diag(mat_r) <- r
-  
-  discounted_utilities <- t(as.matrix(utilities)) %*% mat_r #DISC FACTOR----
-  
-  return(discounted_utilities)
-  
- }
+  return(utility)
+}
 
-period_utility <- sum(disc_cara_utility(coso)) 
+pow_utility_whole <- function(profit_vector, rho=0.5){
+  
+  utility <- sapply(profit_vector, function(x){x^(1-rho)}) %>%
+             sum(na.rm = TRUE)
+
+  
+    return(utility)
+}
 
 #
-certainty_equivalent <- 
-function(period_utility){
+certainty_equivalent_ne <- function(profit_vector, rho=0.005){
   
+  inv_rho<- 1/-rho
+  
+  if(rho==0){
+    ce <- profit_vector %>% unlist() %>% mean()
+    }else{
+
+   suma<- sapply(profit_vector, function(x){exp(-rho*x)}) %>% sum()
+    
+   ce <- log( suma/dim(profit_vector)[1])*(1/(-1*rho)) 
+
+           }
+  
+  return(ce)
 
 } 
 
-certainty_equivalent(utility_level = -200, profit = prof_vec)
+certainty_equivalent_pow <- function(profit_vector, rho=0.5){
+  
+  if(rho==0){
+    ce <- profit_vector %>% unlist() %>% mean()
+  }else{
+  ce <- 
+      (pow_utility_whole(profit_vector=profit_vector, rho = rho)/
+        dim(profit_vector)[1])^(1/(1-rho)) 
+   }
+  
+  return(ce)
+} 
 
 #Put the observed profit (E(X)) and CE to estimate risk premium
 #calculate the risk premium the farmer is 
 #willing to pay to obtain the certainty equivalent
 #Risk_Premium=X-Certainty Equivalent
 
-risk_premium <- 
-function(profit, certainty_equivalent){
+risk_premium_ne <- function(profit_vector, rho=0.005){
   
-  exp_val <- profit %>% sum
+  exp_val <- profit_vector %>% sum
   
-  return(exp_val - certainty_equivalent)   
+  certainty_equivalent <-
+  certainty_equivalent_ne(profit_vector=profit_vector,
+                          rho=rho)
+  
+  risk_premium<- exp_val - certainty_equivalent
+  
+  return(risk_premium)   
+  
+} 
+
+risk_premium_pow <- function(profit_vector, rho=0.5){
+  
+  exp_val <- profit_vector %>% sum
+  
+  certainty_equivalent <-
+    certainty_equivalent_pow(profit_vector=profit_vector,
+                            rho=rho)
+  
+  risk_premium<- exp_val - certainty_equivalent
+  
+  return(risk_premium)   
   
 } 
 
 
- 
-hrus <- profit_data$hru %>% unique
+#Utility Aggregation----
 
-results <- matrix(nrow=length(hrus), ncol=3)
+hru_list <-
+  profit_data %>% select(hru) %>% unique() %>% 
+  unlist() %>% as.list()
+
+ara_par <- seq(0.002, 0.009, by=0.001)
+
+ce_ne <- matrix(nrow = hru_list %>% length(),
+                ncol=8 ) 
+ce_pow <- matrix(nrow = hru_list %>% length(),
+                 ncol=8 ) 
+
+rp_ne <- matrix(nrow = hru_list %>% length(),
+                ncol=8)
+rp_pow <- matrix(nrow = hru_list %>% length(),
+                 ncol=8)
+
+coso <-
+profit_data %>% select(profit_ha,yr ) %>% filter(hru==3)  %>% 
+  as.data.frame() %>%  select(profit_ha)
+
+certainty_equivalent_ne(profit_vector = coso, rho=0.005)
 
 
-for (i in hrus){
-    print(i)
+for(ara in ara_par){
+ for (n_hru in hru_list) {
+  
+ce_ne[match( n_hru, hru_list), match(ara, ara_par)] <-  
+certainty_equivalent_ne(profit_vector = 
+                        profit_data %>% select(profit_ha,yr ) %>%
+                        filter(hru==n_hru)  %>% 
+                        as.data.frame() %>%  select(profit_ha),
+                        rho=ara
+                       )
 
-    prof_vec<- profit_data %>% as.data.frame() %>%
-    filter(hru==i) %>% select(profit_ha) 
 
-    results[match(i, hrus),1] <- farmer_utility(profit=prof_vec)
-    results[match(i, hrus),2] <- sum(prof_vec)
-    results[match(i, hrus),3] <- certainty_equivalent(utility_level = farmer_utility(profit=prof_vec),
-                                                      profit = prof_vec)
+
+rp_ne[match( n_hru, hru_list), match(ara, ara_par)] <-  
+  risk_premium_ne(profit_vector = 
+                            profit_data %>% select(profit_ha,yr ) %>% filter(hru==n_hru)  %>% 
+                            as.data.frame() %>%  select(profit_ha),
+                  rho=ara)
+print(ara)
+print(n_hru)
+
+print(c(ce_ne[match( n_hru, hru_list), match(ara, ara_par)],
+        rp_ne[match( n_hru, hru_list), match(ara, ara_par)]
+      ))  
 
 }
-
-results %>% as.data.frame() %>%
-            rename(Utility=V1,
-                   Profits=V2,
-                   certainty_equivalent=V3) %>% 
-           mutate(risk_premium=Profits-certainty_equivalent) %>% View
-
-results<-
-results %>% as.data.frame() %>% mutate(risk_premium=V2-V3) %>% View
+}
 
 
+ara_par <- seq(0.5, 4, by=0.5)
 
-#Utility Aggregation----
- 
+for(ara in ara_par){
+  for (n_hru in hru_list) {
+    
+    ce_pow[match( n_hru, hru_list), match(ara, ara_par)] <-  
+      certainty_equivalent_pow(profit_vector = 
+                                profit_data %>% select(profit_ha,yr ) %>%
+                                filter(hru==n_hru)  %>% 
+                                as.data.frame() %>%  select(profit_ha),
+                              rho=ara
+      )
+    
+    
+    
+    rp_pow[match( n_hru, hru_list), match(ara, ara_par)] <-  
+      risk_premium_pow(profit_vector = 
+                        profit_data %>% select(profit_ha,yr ) %>% filter(hru==n_hru)  %>% 
+                        as.data.frame() %>%  select(profit_ha),
+                      rho=ara)
+    print(ara)
+    print(n_hru)
+    
+    print(c(ce_pow[match( n_hru, hru_list), match(ara, ara_par)],
+            rp_pow[match( n_hru, hru_list), match(ara, ara_par)]
+    ))  
+    
+  }
+}
+
+resultados_hru <-
+cbind(ce_ne, ce_pow, rp_ne, rp_pow) %>% as.data.frame(); View(resultados_hru)
+
+res_names<- c(paste0("ce_ne_", rac_par), paste0("ce_pow_", rac_par),
+              paste0("rp_ne_", rac_par), paste0("rp_now_", rac_par)
+              )
+
+colnames(resultados_hru)<- res_names
+
 basin_utility <-  
 
 
